5f1312dc6a67375027945d1027b7a0b5da12dbed
zhaodo2109 <155413321+zhaodo2109@users.noreply.github.com>
2024-09-28 13:45:02 -0700

    utilities.py

---
+++ utilities.py
@@ -0,0 +1,13 @@
+import settings
+
+def height_pec(percentage):
+    return(settings.HEIGHT / 100) * percentage
+
+height_pec(25)
+
+#print(height_pec(25))
+
+def width_pec(percentage):
+    return(settings.WIDTH / 100) * percentage
+
+#print(width_pec(40))
